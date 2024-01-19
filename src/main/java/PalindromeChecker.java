eba8a12aa4c749abd8ae6aecbcf9e6bbc7f468e8
jerrythepresident <142940577+jerrythepresident@users.noreply.github.com>
2024-01-19 10:01:36 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -40,12 +40,4 @@ public boolean palindrome(String word)
     String reverse = new StringBuffer(word).reverse().toString();
     return reverse.equals(word);
 }
-public String reverse(String str)
-{
-    StringBuilder reversed = new StringBuilder();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed.append(str.charAt(i));
-        }
-        return reversed.toString();
-}
 }
